b2366ceacec317baafcc21d28f29d464c67b1da2
neel-spartacus <neel.abstract@gmail.com>
2025-06-17 14:54:19 +0200

    Control flow if else

---
+++ controlflow/src/main.rs
@@ -0,0 +1,16 @@
+
+//If Else[If Expression][Else Expression]
+#![allow(warnings)]
+fn main() {
+    let age:u16=18;
+    if age>=18{
+        println!("You can drive a car!");
+    }else{
+        println!("YOu are not eligible to drive a car!");
+    }
+
+    //Using if ina let statement
+    let condition:bool=false;
+    let number=if condition {5} else {6};
+    println!("Number is: {}",number);
+}
